53e58594c5fb6b00d6313ddbd5dd7bca43bdacb5
CHITRON BHATTACHARJEE <162599803+brandchitron@users.noreply.github.com>
2025-06-17 11:30:05 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,14 @@ function startProject() {
 }
 
 startProject();
+const express = require('express');
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("GoatBot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Web server listening on port ${PORT}`);
+});
